cc81695bb0ec3bf0c2fd75c9bb81641ee336e0df
Diego Rodrigues <diejorarr@gmail.com>
2025-04-11 13:36:48 -0500

    add custom conan remote to github workflow

--- .github/scripts/conan-ci-setup.sh
+++ .github/scripts/conan-ci-setup.sh
@@ -15,9 +15,13 @@ mv "$profile" "${profile}.bak"
 sed 's/^\(compiler\.cppstd=\).\{1,\}$/\1'"$std/" "${profile}.bak" > "$profile"
 rm "${profile}.bak"
 
+
 if [ -f conan_cache_save.tgz ]; then
   conan cache restore conan_cache_save.tgz
 fi
+
+conan remote add dpconan https://conan.diejor.tech
+
 conan remove \* --lru=1M -c
 conan install . -b missing
 conan cache save '*/*:*' --file=conan_cache_save.tgz
